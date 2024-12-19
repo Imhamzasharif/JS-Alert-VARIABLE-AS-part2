29ef76a3e282a24b3de94b2f58721f9cf1c0bc7c
Hamza Sharif <160274743+Imhamzasharif@users.noreply.github.com>
2024-12-19 14:08:17 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+// Chapter 01 Alert
+alert("Welcome to my Website");
+
+// Question 02
+alert("Error! Please enter valid password");
+
+// Question 03
+
+alert ("Good, now you will have to write answer of question 4");
+
+// Question 04
+alert ("write a message in sequance. this alert is answer four 04");
+
+// Question 05
+alert ("five k answer k liye browser ka console use krna hain");
+
+document.write("hello how's it going?");
